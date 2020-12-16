25511bc2e108b1847fa5dac099cc24bb7de8acb5
caovicto <caovicto@msu.edu>
2020-12-15 20:19:34 -0500

    added layers and upsampling

--- BgNetwork/model.py
+++ BgNetwork/model.py
@@ -11,18 +11,30 @@ from torch.utils.data import Dataset, Subset, DataLoader, random_split
 
 import time
 
+
 class BgNet(nn.Module):
     def __init__(self):
         super(BgNet, self).__init__()
         self.batch_size = 1
 
-        self.out = nn.Conv2d(in_channels=24, out_channels=12, kernel_size=1)
-
+        print("running network")
+        in_channels = 34
+        self.model = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=128, kernel_size=3),
+            nn.LeakyReLU(),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3),
+            nn.LeakyReLU(),
+            nn.Conv2d(in_channels=128, out_channels=96, kernel_size=3),
+            nn.LeakyReLU(),
+            nn.Conv2d(in_channels=96, out_channels=64, kernel_size=3),
+            nn.LeakyReLU(),
+            nn.Conv2d(in_channels=64, out_channels=3, kernel_size=3),
+            nn.UpsamplingBilinear2d(size=(360, 640))
+        )
 
     def forward(self, x):
-        # Down
-        x = self.out(x)
-
+        x = self.model(x)
+        x = self.upsample(x)
         return x
 
     def save(self, path):
@@ -31,3 +43,5 @@ class BgNet(nn.Module):
     def load(self, path):
         self.load_state_dict(torch.load(path))
         self.eval()
+
+
